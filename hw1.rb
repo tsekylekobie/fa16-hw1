81fc951ac353334faeabb00d444d4648ffd6db6a
tsekylekobie <tse.kylekobie@gmail.com>
2016-09-20 22:46:06 -0700

    HW1 #1-3 Answers

--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,11 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-  (a + b) ** 2
+  (a+b)**2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-  a.sort!.map do |b|
-    b + 1
-  end
+  return a.sort.map{|x| x+1}
 end
 
 def combine_name(first_name, last_name)
